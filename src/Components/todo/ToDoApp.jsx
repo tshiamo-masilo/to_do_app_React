c05a865aa0bcb88d9c847c3d9fb5b89c06225ddc
tshiamo-masilo <63866003+tshiamo-masilo@users.noreply.github.com>
2023-05-05 20:15:03 +0200

    Changes

--- src/Components/todo/ToDoApp.jsx
+++ src/Components/todo/ToDoApp.jsx
@@ -1,10 +1,47 @@
-
+import React, { useState } from 'react'
 export default function ToDoApp(){
     return(
         <div className="ToDoApp">
-
+            Hello World
+            <LoginComponent/>
         </div>
     )
 
 
+}
+
+function LoginComponent(){
+
+    const [username,setUSer] = useState('UserName')
+
+    function handleUsernameChange(event){
+        console.log(event);
+    }
+
+    return(
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label >User Name:</label>
+                <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
+                </div>
+
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password"/>
+                </div>
+                <div>
+                    <button type="button">Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WelcomeComponent(){
+    return(
+        <div className="Welcome">
+            Welcome Component
+        </div>
+    )
 }
